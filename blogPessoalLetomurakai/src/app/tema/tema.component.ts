76e9b7efc6c918b7d18c452d0b965f408c04be0a
Leto Murakai <letocrazy@hotmail.com>
2021-07-02 11:03:17 -0300

    impelemntação do get e post funcionando

--- blogPessoalLetomurakai/src/app/tema/tema.component.ts
+++ blogPessoalLetomurakai/src/app/tema/tema.component.ts
@@ -1,6 +1,9 @@
+import { Usuario } from './../model/Usuario';
+import { TemaService } from './../service/tema.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
 
 @Component({
   selector: 'app-tema',
@@ -9,17 +12,36 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
 
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+
   constructor(
-    private router: Router
+    private router: Router,
+    private temaService: TemaService
   ) { }
 
   ngOnInit(){
-
     if(environment.token == ''){
       alert('Pareçe que sua sessão expirou, faça login novamente! :D')
       this.router.navigate(['/login'])
     }
 
+    this.findAllTema()
+  }
+
+  findAllTema(){
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
+    })
+  }
+
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema) =>{
+      this.tema = resp
+      this.findAllTema()
+      alert('Tema cadastrado com sucesso! :D')
+      this.tema = new Tema()
+    })
   }
 
 }
